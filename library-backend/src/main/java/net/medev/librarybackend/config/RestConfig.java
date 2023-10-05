fff509572abfe5270d4c97901292345f66b8c6f2
Med Faji <meedfaji@gmail.com>
2023-10-05 01:41:54 +0100

    Configure Spring Data Rest

---
+++ library-backend/src/main/java/net/medev/librarybackend/config/RestConfig.java
@@ -0,0 +1,37 @@
+package net.medev.librarybackend.config;
+
+import net.medev.librarybackend.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedMethods = {
+                HttpMethod.DELETE,
+                HttpMethod.PUT,
+                HttpMethod.PATCH,
+                HttpMethod.POST
+        };
+
+        config.exposeIdsFor(Book.class);
+
+        config.getExposureConfiguration()
+                .forDomainType(Book.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods));
+
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
+    }
+
+
+}
