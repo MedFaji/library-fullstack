88d3591ade19fd3ac18e0c3e61e218888a64a33c
Med Faji <meedfaji@gmail.com>
2023-10-05 18:41:51 +0100

    Adding filtering options

--- library-backend/src/main/java/net/medev/librarybackend/dao/BookRepository.java
+++ library-backend/src/main/java/net/medev/librarybackend/dao/BookRepository.java
@@ -1,7 +1,13 @@
 package net.medev.librarybackend.dao;
 
 import net.medev.librarybackend.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContaining(@RequestParam(name = "title") String title, Pageable pageable);
+    Page<Book> findByCategory(@RequestParam(name = "category") String category, Pageable pageable);
 }
